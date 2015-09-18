3b9f9619c18e4e6452f481293a616ff674be7ed4
unknown <jjensen@40034RWADE1241.lcc.usairways.com>
2015-09-18 11:15:43 -0700

    #

---
+++ install_docker.sh
@@ -0,0 +1,4 @@
+wget -qO- https://get.docker.io/gpg | sudo apt-key add -
+sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get install lxc-docker
